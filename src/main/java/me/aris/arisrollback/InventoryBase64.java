729457369d9745b568e982fd9c5de0ec3fee192c
nguyendzvennlmao <tqnmods111@gmail.com>
2026-03-28 05:19:53 +0700

    Create InventoryBase64.java

---
+++ src/main/java/me/aris/arisrollback/InventoryBase64.java
@@ -0,0 +1,41 @@
+package me.aris.arisrollback;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class InventoryBase64 {
+    public static String toBase64(ItemStack[] items) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+            dataOutput.writeInt(items.length);
+            for (ItemStack item : items) {
+                dataOutput.writeObject(item);
+            }
+            dataOutput.close();
+            return Base64Coder.encodeLines(outputStream.toByteArray());
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static ItemStack[] fromBase64(String data) {
+        if (data == null || data.isEmpty()) return new ItemStack[0];
+        try {
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+            ItemStack[] items = new ItemStack[dataInput.readInt()];
+            for (int i = 0; i < items.length; i++) {
+                items[i] = (ItemStack) dataInput.readObject();
+            }
+            dataInput.close();
+            return items;
+        } catch (Exception e) {
+            return new ItemStack[0];
+        }
+    }
+              }
